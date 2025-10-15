6b73968148ca525c136f84db25e77fd1317b902e
Rakesh Telang <134310724+rakesh2971@users.noreply.github.com>
2025-10-15 22:07:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
+    base: '/Vocal_Link/', // <-- Add this line
     server: {
       port: 3000,
       host: '0.0.0.0',
